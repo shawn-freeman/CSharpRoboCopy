f91f6bb1ad8ec0a4f84d7ef4db7e43c12774ed90
Shawn Freeman <magus_zsf@hotmail.com>
2022-04-12 15:51:44 -0400

    Initial Checkin

---
+++ WebApplication1/ClassLibrary1/RobocopyWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class RobocopyWrapper
+    {
+        public bool CallRoboCopy(string source, string dest, string filename, string options)
+        {
+            try
+            {
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.Arguments = $"/C RoboCopy /S {source} {dest} {filename} {options}";
+                    myProcess.StartInfo.FileName = "CMD.EXE";
+                    myProcess.Start();
+                    myProcess.WaitForExit();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WebApplication1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var wrapper = new RobocopyWrapper();
+
+            //Example Useage
+            //var result = wrapper.CallRoboCopy("\"c:\\_repos\\New folder 2\"", "\"c:\\_repos\\New folder\"", "*", "/e");
+            var result = wrapper.CallRoboCopy("{SOURCE}", "{DESTINATION]", "{FILE}", "{OPTIONS}");
+
+            Console.WriteLine(result);
+        }
+    }
+}
